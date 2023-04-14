2e9087288ef453fb1779943e546961b06d3ade5b
stavan30 <stavanpatel107@gmail.com>
2023-04-14 14:24:48 -0700

    First commit chasing clicks

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useEffect } from 'react';
+import { useState } from 'react';
 
 function App() {
+  const [count, setCount] = useState(parseInt(localStorage.getItem('clickCount')) || 0);
+
+  useEffect(() => {
+    localStorage.setItem('clickCount', [count]);
+  })
+
+  const handleClick = () => {
+    setCount(count + 1);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='flex flex-col justify-center items-center bg-gradient-to-b from-green-300 to-slate-200 h-screen relative'>
+      <h1 className='text-5xl'>Chasing The Clicks</h1>
+      <h2 className='text-2xl py-5'>Number of Clicks: {count}</h2>
+      <button className='border-2 border-black rounded h-10 w-max p-2 bg-gradient-to-tr from-green-600 to-green-500 text-black' onClick={handleClick}>Click Me</button>
+      <h3 className='absolute bottom-1 left-auto'>&#169; SESL1 2023 - Stavan Patel</h3>
     </div>
   );
 }
